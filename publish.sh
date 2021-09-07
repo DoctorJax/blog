16463dbf42b70ed7f3552002331a5edec8af159b
DoctorJax <jdl2016nc@gmail.com>
2021-09-07 13:09:29 -0400

    adding a publish script and messing with things

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+printf "\n Updating and publishing site to GitHub \n"
+
+hugo
+
+cd public
+
+git add .
+
+msg="rebuilding site as of $(date +%m-%d-%Y_%T)"
+if [ -n "$*" ]; then
+    msg="$*"
+fi
+
+git commit -m "$msg"
+
+git push origin main
