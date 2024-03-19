e0e812ad8e1559c35716faa6a3f6f1cbe84902b1
DoctorJax <jdl2016nc@gmail.com>
2024-03-18 23:17:09 -0400

    rebuilding site as of 03-18-2024_23:17:09

--- publish.sh
+++ publish.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
 set -e
+set -x
 
 printf "\n Updating and publishing site to GitHub \n"
-
-zola build
-
-cp -rf public/* ../doctorjax.github.io/
-
-cd ../doctorjax.github.io/
 
 msg="rebuilding site as of $(date +%m-%d-%Y_%T)"
 if [ -n "$*" ]; then
     msg="$*"
 fi
 
+zola build
+
+git add .
+
 git commit -m "$msg"
 
 git push
 
-# Updating blog repo too
-cd ../blog/
+cp -rf public/* ../doctorjax.github.io/
 
-git add .
+cd ../doctorjax.github.io/
 
 git commit -m "$msg"
 
